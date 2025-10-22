b6abf375c30a748c0dbcc932e5bda2661fac51d3
Cesar Urbina <cu046481@gmail.com>
2025-10-22 09:11:30 -0500

    Añadi pedidos

--- src/utils/screenName.js
+++ src/utils/screenName.js
@@ -1,27 +1,36 @@
-const RestaurantStack={
-    tab: "RestaurantsTab",
-    restautants:"menus",
-    information: "Information",
+const RestaurantStack = {
+  tab: "RestaurantsTab",
+  restaurants: "menus",
+  information: "Information",
 };
-const FavoriteStack= {
-    tab: "FavoriteTab",
-    List:"ListFavorite",
-};
-const AccountStack ={
-    tab: "AccountTab",
-    List: "ListAccount",
-    login: "Login",
-    register:"Register",
-    forgotPassword: "ForgotPassword",
-};
-const MenuListStack= {
-    tab: "MenuListTab",
-    List: "MenuTab",
-};
-export const screen ={
-    restaurant: RestaurantStack,
-    favorites: FavoriteStack,
-    account:AccountStack,
-    menulist: MenuListStack
 
+const FavoriteStack = {
+  tab: "FavoriteTab",
+  list: "ListFavorite",
+};
+
+const AccountStack = {
+  tab: "AccountTab",
+  list: "ListAccount",
+  login: "Login",
+  register: "Register",
+  forgotPassword: "ForgotPassword",
+};
+
+const MenuListStack = {
+  tab: "MenuListTab",
+  list: "MenuTab",
+};
+
+const PedidosStack = {
+  tab: "PedidosTab",
+  list: "PedidosScreen",
+};
+
+export const screen = {
+  restaurant: RestaurantStack,
+  favorites: FavoriteStack,
+  account: AccountStack,
+  menulist: MenuListStack,
+  pedidos: PedidosStack,
 };
