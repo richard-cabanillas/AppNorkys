a5e37931c3b2d8c3054ea3934526e99f51135510
Cesar Urbina <cu046481@gmail.com>
2025-11-04 16:52:34 -0500

    Cuenta

--- src/navigation/AccountStack.js
+++ src/navigation/AccountStack.js
@@ -2,7 +2,7 @@ import{createNativeStackNavigator} from "@react-navigation/native-stack";
 import{AccountScreen} from  "../Screens/Account/AccountScreen";
 import{screen} from "../utils";
 import{LoginScreen} from "../Screens/Account/LoginScreen/LoginScreen";
-import{ResgisterScreen} from "../Screens/Account/RegisterScreen/ResgisterScreen";
+import{RegisterScreen} from "../Screens/Account/RegisterScreen/RegisterScreen";
 import{ForgotPassword} from "../Screens/Account/ForgotPassword/ForgotPassword";
 
 const Stack = createNativeStackNavigator();
@@ -11,13 +11,13 @@ export function AccountStack(){
     return(
         <Stack.Navigator>
 
-            <Stack.Screen name={screen.account.List} component={AccountScreen} 
+            <Stack.Screen name={screen.account.list} component={AccountScreen} 
             options={{title:"Configurar cuenta"}}/>
 
             <Stack.Screen name={screen.account.login} component={LoginScreen} 
             options={{title:"Iniciar Sesion"}}/>
             
-            <Stack.Screen name={screen.account.register} component={ResgisterScreen} 
+            <Stack.Screen name={screen.account.register} component={RegisterScreen} 
             options={{title:"Crea tu cuenta"}}/>
             
             <Stack.Screen name={screen.account.forgotPassword} component={ForgotPassword} 
